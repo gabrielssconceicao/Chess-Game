e8cdb69600304e7298fb289622cf2e96c66c7d52
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-20 12:06:27 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,6 +47,20 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position){
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the Board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
     public boolean positionExists(Position position) {
         return positionExists(position.getRow(),position.getColumn());
     }
@@ -61,4 +75,6 @@ public class Board {
         }
         return piece(position) != null;
     }
+
+
 }
